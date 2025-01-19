ffd014f4d9b7a66e16df4a84a82c2639e9cc6c7a
slavk <slavka.cherenkov@gmail.com>
2025-01-19 22:50:28 +0300

    copy repositories

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,23 +1,28 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
 
+    UserDaoJDBCImpl userDao = new UserDaoJDBCImpl("jdbc:mysql://localhost:3306/users");
+
+    public void createUsersTable() {
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
+        userDao.removeUserById(id);
 
     }
 
@@ -26,6 +31,6 @@ public class UserServiceImpl implements UserService {
     }
 
     public void cleanUsersTable() {
-
+        userDao.cleanUsersTable();
     }
 }
